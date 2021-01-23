620531e63ab6452d3ec2b110c18cd1b22918c728
Chase Parks <chase.parks94@gmail.com>
2021-01-23 16:17:58 -0700

    Add axios, hit lambda function to check domain availability

--- src/App.js
+++ src/App.js
@@ -1,22 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState, useEffect} from 'react';
+import axios from 'axios';
 
 function App() {
+  const [domainName, setDomainName] = useState('');
+  const [domainAvailability, setDomainAvailability] = useState('');
+  const [search, setSearch] = useState('');
+
+  useEffect(async () => {
+    const checkDomainNameAvailability = async () => {
+      const result = await axios(
+        `https://j7thv6xq6e.execute-api.us-west-1.amazonaws.com/prod/MyServerlessWebsite?domain=${search}`
+      )
+      console.log(result)
+      setDomainAvailability(result.data['Availability']);
+    };
+    checkDomainNameAvailability();
+  }, [search])
+  
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          lambda try
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h2>siteform</h2>
+        <p>domain:</p> <input value={domainName} onChange={event => setDomainName(event.target.value)}/>
+        <br></br>
+        <button onClick={event => setSearch(domainName)}>check availability</button>
+        <h4>{domainName} is {domainAvailability}</h4>
       </header>
     </div>
   );
